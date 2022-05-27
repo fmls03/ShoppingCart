aa4b168511c169e725a2603793c4bb307dbbbc6b
fmls03 <melisfrancesco.003@gmail.com>
2022-05-28 01:06:21 +0200

    migliorato design

---
+++ frontend2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <router-view/>
+  </div>
+
+</template>
+
+
+<style>
+
+.app{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Courier New Lucida Console Monaco", "Monospace";
+  background: rgb(31,31,37);
+  background: -moz-linear-gradient(0deg, rgba(31,31,37,1) 33%, rgba(60,73,84,1) 100%);
+  background: -webkit-linear-gradient(0deg, rgba(31,31,37,1) 33%, rgba(60,73,84,1) 100%);
+  background: linear-gradient(0deg, rgba(31,31,37,1) 33%, rgba(60,73,84,1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#1f1f25",endColorstr="#3c4954",GradientType=1);  
+}
+
+#app{
+  height: 100%;
+}
+</style>
--- frontend2/src/components/ShoppingCart.vue
+++ frontend2/src/components/ShoppingCart.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container">
         <div id="div-title">
-            <p id="title">Carrello della spesa</p>
+            <p id="title">Shopping Cart</p>
         </div>
         <table class="table">
             <tr>
-                <th id="columns">Prodotto</th>
-                <th id="columns">Comprato</th>
+                <th id="columns">Products</th>
+                <th id="columns">Bought</th>
             </tr>
 
             <tr v-for="(product, index) in products" :key="index" >
@@ -19,7 +19,7 @@
 
         <div id="div-add-product">
             <p id="text-add-product">Add product:</p>
-            <input type="text" name="add-product" id="add-product" placeholder="Add product" v-model="newName">
+            <input type="text" name="add-product" id="add-product" placeholder="Product" v-model="newName">
             <input type="button" name="btn-add-product" id="btn-add-product" value="Add" v-on:click="addProduct">
         </div>
     </div>
@@ -116,35 +116,50 @@ export default {
     width: 220px;
     height: 30px;
     border-radius: 5px;
-    background-color: rgb(100, 100, 100, 0.4);
-    font-weight: bold;
+    background: rgb(46,51,56);
+    background: -moz-linear-gradient(0deg, rgba(46,51,56,1) 0%, rgba(60,73,84,1) 100%);
+    background: -webkit-linear-gradient(0deg, rgba(46,51,56,1) 0%, rgba(60,73,84,1) 100%);
+    background: linear-gradient(0deg, rgb(43, 50, 56) 0%, rgb(56, 68, 79) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#2e3338",endColorstr="#3c4954",GradientType=1);
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 7px 29px 0px;  
+    color: white;
 }
 
-#text-title{
-    color: black;
-}
 
 .table{
     margin-top: 10%;
     text-align: center;
-    background-color: rgb(150, 150, 150, 0.6);
     border-radius: 6px;
+    background: rgb(36,41,49);
+    background: -moz-linear-gradient(0deg, rgba(36,41,49,1) 0%, rgba(39,47,54,1) 100%);
+    background: -webkit-linear-gradient(0deg, rgba(36,41,49,1) 0%, rgba(39,47,54,1) 100%);
+    background: linear-gradient(0deg, rgb(30, 34, 41) 0%, rgb(44, 53, 62) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#242931",endColorstr="#272f36",GradientType=1);
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 7px 29px 0px;  
+
 }
 
 th{
     width: 200px;
     font-size: smaller;
     font-weight: normal;
+    color: white;
 }
 
 #columns{
-    border-bottom:1px solid rgba(0, 0, 0, 0.328);
     font-weight: bold;
 
 }
 
 #div-add-product{
-    background-color: rgb(150, 150, 150, 0.6);
+    background: rgb(36,41,49);
+    background: -moz-linear-gradient(0deg, rgba(36,41,49,1) 0%, rgb(37, 45, 52) 100%);
+    background: -webkit-linear-gradient(0deg, rgba(36,41,49,1) 0%, rgba(39,47,54,1) 100%);
+    background: linear-gradient(0deg, rgb(30, 34, 41) 0%, rgb(44, 53, 62) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#242931",endColorstr="#272f36",GradientType=1);
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 7px 29px 0px;  
+
+    color:white;
     border-radius: 5px;
     display: flex;
     flex-direction: row;
@@ -163,34 +178,39 @@ th{
 
 #add-product{
     margin-right: 20px;
+    height: 14px;
+    border: 0;
     border-radius: 4px;
-    background-color: aliceblue;
-    color:black;
+    background: rgb(54, 62, 66);
+    color:rgb(255, 255, 255);
     outline: none;
-    opacity: 0.7;
     text-decoration: none;;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 7px 29px 0px;  
+
 }
 
 #add-product:hover{
-    border-color: cornflowerblue;
-    box-shadow: 0 0 5px cornflowerblue;
+    border-color: rgb(128, 169, 244);
+    box-shadow: 0 0 5px rgb(120, 164, 245);
 }
 
 #btn-add-product{
     width: 50px;
     border-radius: 4px;
     border: 2px;
-    background-color:dodgerblue;
+    background: rgb(58, 76, 84);
     font-size: smaller;
-    font-weight: bold;
     opacity: 0.8px;
     height: 15px;
+    color: white;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 7px 29px 0px;  
+
 }
 
 #btn-add-product:hover{
     opacity: 1;
-    border-color: cornflowerblue;
-    box-shadow: 0 0 5px cornflowerblue;
+    border-color: rgb(128, 169, 244);
+    box-shadow: 0 0 5px rgb(120, 164, 245);
 }
 
 #delete-column{
